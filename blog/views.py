3beaa209dd3e7cb52634e6286d1e10a246827f98
Kelsey <kelsey@edmodo.com>
2014-11-01 15:06:31 -0700

    View one post at a time

--- blog/views.py
+++ blog/views.py
@@ -6,9 +6,10 @@ from blog import app
 from database import session
 from models import Post
 
+
 @app.route("/")
 @app.route("/page/<int:page>")
-def posts(page=1, paginate_by=10):
+def pages(page=1, paginate_by=10):
     # Zero-indexed page
     page_index = page - 1
 
@@ -25,13 +26,42 @@ def posts(page=1, paginate_by=10):
     posts = posts.order_by(Post.datetime.desc())
     posts = posts[start:end]
 
-    return render_template("posts.html",
+    return render_template("pages.html",
         posts=posts,
         has_next=has_next,
         has_prev=has_prev,
         page=page,
         total_pages=total_pages
+    )
+
+
+@app.route("/")
+@app.route("/post/<int:post>")
+def posts(post=1, paginate_by=1):
+    # Zero-indexed  
+    page_index = post #- 1
+
+    count = session.query(Post).count()
+
+    start = page_index * paginate_by
+    end = start + paginate_by
+
+    total_pages = (count - 1) / paginate_by + 1
+    has_next = page_index > 0
+    has_prev = page_index < total_pages - 1
+
+    posts = session.query(Post)
+    posts = posts.order_by(Post.datetime)
+    posts = posts[start:end]
+
+    return render_template("posts.html",
+        posts=posts,
+        has_next=has_next,
+        has_prev=has_prev,
+        post=post,
+        total_pages=total_pages
     )    
+
 
 @app.route("/post/add", methods=["GET"])
 def add_post_get():
